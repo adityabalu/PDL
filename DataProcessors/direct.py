6e479b71ace4d4a814adec51eaf3952c178dd765
Aditya\Aditya home <baditya@iastate.edu>
2019-03-05 11:32:44 -0600

    commit simple code for mnist_cnn and direct loading of mnist data

---
+++ DataProcessors/direct.py
@@ -0,0 +1,5 @@
+from mnist import train_images, test_images, train_labels, test_labels
+
+if __name__ == '__main__':
+    X_train, Y_train = train_images(), train_labels()
+    X_test, X_test = test_images(), test_labels()
